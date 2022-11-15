1bf06b0bd3a0e2060555123438be3ff59f1ff4ac
AMIT SINGH BISHT <69075588+amit-singh-bisht@users.noreply.github.com>
2022-11-15 12:53:38 +0530

    Add files via upload

---
+++ lib/selenium_webdriver_helper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "selenium_webdriver_helper/version"
+
+module SeleniumWebdriverHelper
+  class Error < StandardError; end
+  # Your code goes here...
+end
